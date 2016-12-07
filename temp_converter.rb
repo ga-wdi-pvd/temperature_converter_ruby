2e19f6daa478274bc97fba83f4ea2359d5bf5ec9
yourUsername <your_email@example.com>
2016-12-07 08:57:50 -0500

    Attempted HW

--- temp_converter.rb
+++ temp_converter.rb
@@ -1,1 +1,14 @@
+#prompt user for temp and unit type
+puts "What is the temp?"
+temp = gets.chomp
+puts "f, C, or K?"
+unit = gets.chomp
 
+def convert_temp temp, unit
+  if (unit == 'fahrenheit' or unit =='F')
+    return temp.round((temp - 32) * 5/9) + 'C'
+  else
+    return temp.round(temp +273.15) + 'C'
+  end
+end
+git add 

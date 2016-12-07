c833e2b235b9d746a976dbafde00fc3d5fa58369
awanderlyss <alyssa.felix7@gmail.com>
2016-12-06 21:51:34 -0500

    Define method to convert temp

--- temp_converter.rb
+++ temp_converter.rb
@@ -5,6 +5,29 @@ puts "Enter a temperature to convert: "
 starting_temp = gets.chomp.to_i
 
 puts "Enter a temperature unit (f, C, K):"
-temp_unit = gets.chomp
+temp_unit = gets.chomp.to_s
 
-#define (def) method that takes starting
+# define (def) method that takes starting
+def conver_temp (starting_temp, temp_unit)
+  if temp_unit == 'f'
+    c = (starting_temp - 32) * 5 / 9
+    k = (starting_temp - 32) * 5 / 9 + 273.15
+    puts "fahrenheit: #{starting_temp}"
+    puts "to Celsius: #{c}"
+    puts "to Kelvin: #{k}"
+  elsif temp_unit == 'C'
+    f = (starting_temp * 9 / 5) + 32
+    k = starting_temp + 273.15
+    puts "Celsius: #{starting_temp}"
+    puts "to fahrenheit: #{f}"
+    puts "to Kelvin: #{k}"
+  else
+    f = (starting_temp - 273.15) * 9 / 5 + 32
+    c = starting_temp - 273.15
+    puts "Kelvin: #{starting_temp}"
+    puts "to fahrenheit: #{f}"
+    puts "to Celsius: #{c}"
+  end
+end
+
+conver_temp(starting_temp, temp_unit)

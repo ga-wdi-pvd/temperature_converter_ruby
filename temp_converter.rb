91666169fafc452997889f5ba835658ef1feb905
awanderlyss <alyssa.felix7@gmail.com>
2016-12-06 21:11:43 -0500

    Create vars to store user input

--- temp_converter.rb
+++ temp_converter.rb
@@ -1,1 +1,10 @@
+# two user inputs (gets) and store them in var
+# starting temperature value
+# temoerature unit (e.g f, C, K)
+puts "Enter a temperature to convert: "
+starting_temp = gets.chomp.to_i
 
+puts "Enter a temperature unit (f, C, K):"
+temp_unit = gets.chomp
+
+#define (def) method that takes starting

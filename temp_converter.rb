7d62e8fb0e3f0fba47f8d94bca45e1078f7bfd2a
murphypicard <ayaghsizian@gmail.com>
2016-12-07 08:57:48 -0500

    Ara ruby temp converter

--- temp_converter.rb
+++ temp_converter.rb
@@ -1,1 +1,28 @@
+print 'What is the temperature?'
 
+degrees = gets.chomp.to_i
+
+print 'type lowercase f or c for fahrenheit or celsius, please:'
+
+typeOfDegrees = gets.chomp
+
+puts 'Now, the amazing Ara, will convert it for u.'
+
+def fToC (degrees)
+  celsius = (degrees - 32) * (5.0/9)
+  puts "It is now #{celsius} degrees celsius!  Ta da!"
+  return celsius
+end
+
+def cToF (degrees)
+  fahrenheit = degrees * 9 / 5 + 32
+  puts "It is now #{fahrenheit} degrees fahrenheit!  Ta da"
+  return fahrenheit
+end
+
+
+if typeOfDegrees == "f"
+  fToC(degrees)
+elsif typeOfDegrees == "c"
+  cToF(degrees)
+end
